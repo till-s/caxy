a391ead81846174977a46e9c87473cdd8bab96e5
till <till.straumann@alumni.tu-berlin.de>
2012-05-24 22:19:55 -0500

    - separated PktChannel class into PktOutChannel and PktInpChannel.  Added PktOutStrmChannel class.
       All for sake of I/O to a sub-process (spawned by Runtime.exec()) which provides
       only InputStream and OutputStream objects, respectively. Found no way to make
       these into ByteChannels; but only ReadableByteChannel/WritableByteChannel.
       This mandates the creation of separate classes for input and output.
    
       When piping I/O to a sub-process (spawned via Runtime.exec()) I discovered that
       I need to 'flush()' the output so that it doesn't get stuck in the pipes.
       Unfortunately, flushing a WritableByteChannel is not possible. Therefore
       we added a separate class: PktOutStrmChannel which keeps a reference to the
       the underlying OutputStream and flushes it from 'putBuf' (a overridden method).

--- caxy/PktChannel.java
+++ caxy/PktChannel.java
@@ -3,24 +3,20 @@ package caxy;
 import java.nio.ByteBuffer;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.ByteChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.channels.DatagramChannel;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 
-class PktChannel {
-	protected ByteChannel chnl;
-
-	class IncompleteBufferReadException extends Exception {
-		int got, wanted;
-		protected IncompleteBufferReadException(int got_in, int wanted_in)
-		{
-			got = got_in; wanted = wanted_in;
-		}
-	}
+class PktOutChannel {
+	protected WritableByteChannel chnl;
 
 	class IncompleteBufferWrittenException extends Exception {
 		int put, wanted;
@@ -30,31 +26,14 @@ class PktChannel {
 		}
 	}
 
-
-	public PktChannel(ByteChannel chnl_in)
+	public PktOutChannel(WritableByteChannel chnl_in)
 	{
-		chnl = chnl_in;
+		chnl  = chnl_in;
 	}
 
-	public static PktChannel getStdout()
+	public static PktOutChannel getStdout()
 	{
-		return new PktChannel( (new FileOutputStream(java.io.FileDescriptor.out)).getChannel());
-	}
-
-	public static PktChannel getStdin()
-	{
-		return new PktChannel( (new FileInputStream(java.io.FileDescriptor.in)).getChannel());
-	}
-
-
-	public void getPkt(ByteBuffer b)
-		throws IOException, IncompleteBufferReadException
-	{
-		int here = b.position();
-		while ( b.remaining() > 0 && chnl.read(b) > 0 )
-			;
-		if ( b.remaining() > 0 )
-			throw new IncompleteBufferReadException(b.position() - here, b.limit());
+		return new PktOutChannel( (new FileOutputStream(java.io.FileDescriptor.out)).getChannel());
 	}
 
 	protected void putBuf(ByteBuffer b, SocketAddress peer)
@@ -66,6 +45,7 @@ class PktChannel {
 			;
 		if ( b.remaining() > 0 )
 			throw new IncompleteBufferWrittenException(b.position() - here, b.limit());
+
 	}
 
 	protected synchronized void putPkt(WrapHdr wHdr, SocketAddress peer)
@@ -89,11 +69,57 @@ class PktChannel {
 		wHdr.out(this, peer);
 		caPkt.out(this, peer);
 	}
+}
 
-	public static PktChannel open(boolean inside, int port)
+/* Subclass of PktOutChannel which makes sure the underlying
+ * OutputStream is flushed after writing packets.
+ * This seems to be necessary when we write to a sub-process'es
+ * stdin obtained via Process.getOutputStream().
+ *
+ * Very ugly!
+ *
+ */
+class PktOutStrmChannel extends PktOutChannel {
+
+	protected OutputStream        ostrm;
+
+	public PktOutStrmChannel(OutputStream ostrm)
+	{
+		super( Channels.newChannel( ostrm ) );
+		this.ostrm = ostrm;
+	}
+
+	protected void putBuf(ByteBuffer b, SocketAddress peer)
+		throws IOException, IncompleteBufferWrittenException
+	{
+		super.putBuf(b, peer);
+		{
+			/* Ugly hack: If I stitch together a pipe from the outside to the inside
+			 * using the OutputStream/InputStreams from Process (when spawning a new
+			 * process) then the communication hangs -- unless I flush the output stream
+			 * after we send :-(. Unfortunately there is no way to flush
+			 * a WritableByteChannel -- hence we can't do the flushing just using the
+			 * channel.
+			 * Thus, we keep a handle to the output stream around and flush manually
+			 * here. How ugly!
+			 * 
+			 * Note that this is not necessary for any other kind of stream:
+			 *   - TCP connection
+			 *   - FileInputStream/FileOutputStream via getStin
+			 */
+		}
+		ostrm.flush();
+	}
+}
+
+/* Bidirectional channel with underlying socket */
+class PktBidChannel {
+
+	protected ByteChannel bc;
+
+	public PktBidChannel(boolean inside, int port)
 		throws IOException
 	{
-	ByteChannel bc;
 	ServerSocketChannel sc;
 		if ( inside ) {
 			InetSocketAddress sa = new InetSocketAddress( port );
@@ -105,7 +131,58 @@ class PktChannel {
 			InetSocketAddress sa = new InetSocketAddress( "localhost", port );
 			bc = SocketChannel.open( sa );
 		}
-		
-		return new PktChannel( bc );
+	}
+
+	PktInpChannel getPktInpChannel()
+	{
+		return new PktInpChannel((ReadableByteChannel)bc);
+	}
+
+	PktOutChannel getPktOutChannel()
+	{
+		return new PktOutChannel((WritableByteChannel)bc);
+	}
+}
+
+class PktInpChannel {
+	protected ReadableByteChannel chnl;
+
+	class IncompleteBufferReadException extends Exception {
+		int got, wanted;
+		protected IncompleteBufferReadException(int got_in, int wanted_in)
+		{
+			got = got_in; wanted = wanted_in;
+		}
+	}
+
+	class IncompleteBufferWrittenException extends Exception {
+		int put, wanted;
+		protected IncompleteBufferWrittenException(int put_in, int wanted_in)
+		{
+			put = put_in; wanted = wanted_in;
+		}
+	}
+
+
+	public PktInpChannel(ReadableByteChannel chnl_in)
+	{
+		chnl = chnl_in;
+	}
+
+	public static PktInpChannel getStdin()
+	{
+		return new PktInpChannel( (new FileInputStream(java.io.FileDescriptor.in)).getChannel());
+	}
+
+
+	public void getPkt(ByteBuffer b)
+		throws IOException, IncompleteBufferReadException
+	{
+		int here = b.position();
+		int got;
+		while ( b.remaining() > 0 && (got = chnl.read(b)) > 0 )
+			/* nothing else to do */;
+		if ( b.remaining() > 0 )
+			throw new IncompleteBufferReadException(b.position() - here, b.limit());
 	}
 }
