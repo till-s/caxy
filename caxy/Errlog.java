35687766d6608434c45047531c1e58bb5272f187
till <till.straumann@alumni.tu-berlin.de>
2012-05-29 12:29:00 -0500

    - had forgotten to add Errlog.java to git

---
+++ caxy/Errlog.java
@@ -0,0 +1,69 @@
+package caxy;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+class Errlog extends Thread {
+
+	protected InputStream  is;
+	protected OutputStream os;
+
+	public Errlog(InputStream is)
+	{
+		this(is, System.err);
+	}
+
+	public void start( Class c )
+	{
+		/* start thread only if this constructor is not called
+		 * from a subclass (so that a subclass could do more
+		 * initialization before calling start)
+		 */
+		if ( this.getClass().equals( c ) ) {
+			super.start();
+		}
+	}
+
+	public Errlog(InputStream is, OutputStream os)
+	{
+		this.is = is;
+		this.os = os;
+
+		start( Errlog.class );
+	}
+
+	public void run()
+	{
+		byte []buf=new byte[1];
+		try {
+			while ( 1 == is.read(buf) ) {
+				os.write(buf);
+			}
+		} catch (IOException e) {
+			e.printStackTrace( System.err );
+		} finally {
+			System.err.println("Errlog: terminated");
+		}
+	}
+
+	public static void main(String []args)
+		throws IOException
+	{
+		if ( args.length > 0 ) {
+			Process   p = Runtime.getRuntime().exec(args);
+			OutputStream s;
+			new Errlog(p.getErrorStream());
+			s=p.getOutputStream();
+			s.write('H');
+			s.write('E');
+			s.write('L');
+			s.write('O');
+			s.write('\n');
+			s.flush();
+			System.err.println("FLUSHED");
+		} else {
+			new Errlog(System.in, System.err);
+		}
+	}
+}
