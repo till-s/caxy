7754cde5b27b011207718d4aecb10d074de0c4d8
till <till.straumann@alumni.tu-berlin.de>
2012-05-28 23:59:29 -0500

    - use generic Exception message; added dump of raw header contents
       Special debug flag enables dumping of the raw WrapHdr buffer
       contents.

--- caxy/WrapHdr.java
+++ caxy/WrapHdr.java
@@ -15,6 +15,8 @@ class WrapHdr {
 	protected static final int WRAP_SPORT_OFF = 12;
 	protected static final int WRAP_CPORT_OFF = 14;
 
+	public    static final int DEBUG_RAWBUF   = CaxyConst.DEBUG_SPEC << 1;
+
 	public int get_n_ca()  { b.position(WRAP_N_CA_OFF);  return (b.getShort()) & 0xffff; }
 	public int get_saddr() { b.position(WRAP_SADDR_OFF); return b.getInt(); }
 	public int get_caddr() { b.position(WRAP_CADDR_OFF); return b.getInt(); }
@@ -71,11 +73,28 @@ class WrapHdr {
 		System.err.format( "Client : %d.%d.%d.%d\n", b.get() & 0xff, b.get() & 0xff, b.get() & 0xff, b.get() & 0xff);
 		System.err.format( "Srv Prt: %d\n", (b.getShort()&0xffff));
 		System.err.format( "Clt Prt: %d\n", (b.getShort()&0xffff));
+
+		if ( (debug & DEBUG_RAWBUF) != 0 ) {
+			byte v;
+
+			b.rewind();
+			System.err.println("Raw Contents:");
+			while ( b.remaining() > 0 ) {
+				v = b.get();
+				System.err.format( "0x%02x '%c' ", v, (char)v );
+			} 
+			System.err.println();
+		}
+
 		b.position(opos);
 	}
 
 	class CaxyBadVersionException extends Exception {
 		public final int badVersion;
-		protected CaxyBadVersionException(int version) { badVersion = version; }
+		protected CaxyBadVersionException(int version)
+		{
+			super( "Bad CATUN protocol version: " + version );
+			badVersion = version;
+		}
 	}
 }
