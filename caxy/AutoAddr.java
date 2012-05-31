2a2e260a271bfb1b6c73304f2ad91e84b54c76f2
till <till.straumann@alumni.tu-berlin.de>
2012-05-31 15:46:12 -0500

    - implemented detection of broadcast addresses

--- caxy/AutoAddr.java
+++ caxy/AutoAddr.java
@@ -2,7 +2,11 @@ package caxy;
 
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.net.SocketException;
+import java.net.InterfaceAddress;
 import java.net.InetAddress;
 
 class AutoAddr {
@@ -10,21 +14,38 @@ class AutoAddr {
 	{
 	}
 
-	public static void getList()
+	public static InetAddress [] getList(boolean debug)
 	{
-	Enumeration      ifs, ads;
+	Enumeration      ifs;
+	ListIterator     adi;
 	NetworkInterface nif;
-	InetAddress      ina;
+	InterfaceAddress ifa;
+	InetAddress      bca;
+	LinkedList<InetAddress> bcl = new LinkedList<InetAddress>();
+	InetAddress []   rval;
+	int              i;
+
+		if ( debug )
+			System.err.println("Assembling 'auto address list'");
 
 		try {
 		for ( ifs = NetworkInterface.getNetworkInterfaces(); ifs.hasMoreElements(); ) {
 			nif = (NetworkInterface)ifs.nextElement();
 
-			System.err.println("IF: "+nif.getDisplayName());
+			if ( debug )
+				System.err.println("Checking interface: "+nif.getDisplayName());
 
-			for ( ads = nif.getInetAddresses(); ads.hasMoreElements(); ) {
-				ina = (InetAddress)ads.nextElement();
-				System.err.println("  Addr: " + ina);
+			for ( adi = nif.getInterfaceAddresses().listIterator(); adi.hasNext(); ) {
+				ifa = (InterfaceAddress)adi.next();
+				bca = ifa.getBroadcast();
+
+				if ( debug ) {
+					System.err.println("  Address:     " + ifa);
+					System.err.println("    Broadcast: " + (null == bca ? "NONE" : bca));
+				}
+
+				if ( null != bca )
+					bcl.add( bca );	
 			}
 
 		}
@@ -32,10 +53,18 @@ class AutoAddr {
 			System.err.println("No interfaces found: "+e.getMessage());
 			e.printStackTrace();
 		}
+
+		rval = new InetAddress [bcl.size()];
+
+		for ( adi = bcl.listIterator(), i = 0; adi.hasNext(); i++ ) {
+			rval[i] = (InetAddress)adi.next();
+		}
+
+		return rval;
 	}
 
 	public static void main(String []args)
 	{
-		getList();
+		getList(true);
 	}
 }
