37de35cfb080b65fb821cc09758b2c96228e7f11
till straumann <till.straumann@alumni.tu-berlin.de>
2019-05-09 10:03:05 -0700

    README.java: converted to markdown

--- README.java
+++ README.java
@@ -1,5 +1,4 @@
-INTRODUCTION
-------------
+# INTRODUCTION
 
 caxy is now also available as an implementation in java.
 This means that you don't have to compile anything but
@@ -9,69 +8,69 @@ supporting java (>= 1.6).
 For general information about caxy and how to use it
 consult the general README.
 
-Authorship
-----------
+## Authorship
+
 caxy was created by Till Straumann <strauman@slac.stanford.edu>
 
-NOTE about ProxifiedSocketChannel.java
---------------------------------------
+## Html documentation
+Can be found [here](https://till-s.github.io/caxy)
 
- The 'ProxifiedSocketChannel.java' file implements a class to
+## NOTE about ProxifiedSocketChannel.java
+
+ The `ProxifiedSocketChannel.java` file implements a class to
  enhance the CAJ native java CA-client implementation.
  It adds support for SOCKS4/SOCKS5.
   
  Note that this file is NOT directly RELATED or USED by caxy.
  It is meant to be used to patch CAJ.
 
-RUNNING java caxy
------------------
+## RUNNING java caxy
 
 caxTo run java caxy you need jvm >= 1.6 and you start it
 
-java -jar <path>/caxy.jar
+    java -jar <path>/caxy.jar
 
 The commandline options are mostly the same as for the C version
 (see caxy.html or run caxy with '-h') with the following exceptions:
 
- -S    : 'server' mode runs always in the foreground.
- -f    : ignored.
+     -S    : 'server' mode runs always in the foreground.
+     -f    : ignored.
+    
+     -n    : reverse DNS lookup of IP addresses (debugging mode) is 
+             not supported. All addresses are dumped numerically. The
+             -n option is not recognized.
+    
+     -v    : the java version only supports CATUN protocol version 3.
+             There is no option to select the protocol version.
 
- -n    : reverse DNS lookup of IP addresses (debugging mode) is 
-         not supported. All addresses are dumped numerically. The
-         -n option is not recognized.
+     -P    : when using a dedicated tunnel port (as opposed to STDIO)
+             in 'inside' mode then bind to 'ANY' address. This
+             means that connections from anywhere are possible. By default
+             caxy binds to a local address so that only connections from
+             the machine where caxy is running are possible.
 
- -v    : the java version only supports CATUN protocol version 3.
-         There is no option to select the protocol version.
-
- -P    : when using a dedicated tunnel port (as opposed to STDIO)
-         in 'inside' mode then bind to 'ANY' address. This
-         means that connections from anywhere are possible. By default
-         caxy binds to a local address so that only connections from
-         the machine where caxy is running are possible.
-
-BUILDING java caxy
-------------------
+## BUILDING java caxy
 
 If you decide to hack around and you want to rebuild the
 java version then read this section. If you just want to use
-caxy then the pre-built 'caxy.jar' should be enough on any
+caxy then the pre-built `caxy.jar` should be enough on any
 platform that supports at least java-1.6 (you can rebuild
-'caxy.jar' under 1.5. This will work, too, but you won't
+`caxy.jar` under 1.5. This will work, too, but you won't
 have auto_addr_list support.)
 
 For building the java version you need jdk and ant.
-Simply chdir to the top directory (holding the file 'build.xml')
+Simply chdir to the top directory (holding the file `build.xml`)
 and issue 
 
     ant
 
-To clean up select the 'clean' target
+To clean up select the `clean` target
 
     ant clean
 
-ACKNOWLEDGEMENT/LICENSE
------------------------
-java caxy uses the GNU 'Getopt' class which is released
+## ACKNOWLEDGEMENT/LICENSE
+
+java caxy uses the GNU `Getopt` class which is released
 under the LGPL and distributed in source (and compiled)
 form with caxy.
 When using the pre-built binaries then the respective terms
